f15ab3772777fb104291075fbb1b550410f38e32
archupatil-90 <73097336+archupatil-90@users.noreply.github.com>
2021-05-11 17:32:22 +0530

    Add files via upload

---
+++ javascript/style.js
@@ -0,0 +1,14 @@
+function menu1(){
+    document.getElementById("id2").style.display="block";
+    document.getElementById("id1").style.display="none";
+    document.getElementById("id3").style.display="block";
+
+
+}
+function cross(){
+    document.getElementById("id1").style.display="block";
+    document.getElementById("id2").style.display="none";
+    document.getElementById("id3").style.display="none";
+
+
+}
